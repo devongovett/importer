2a441374f409e55ef9124681ae8c3dd970c91aaa
Andrew Kelley <superjoe30@gmail.com>
2012-07-05 21:49:37 -0400

    use optparse to parse args

--- cmd.js
+++ cmd.js
@@ -2,26 +2,59 @@
 
 var http = require('http'),
     compile = require('./import'),
-    fs = require('fs');
-    
-var mainfile = process.argv[2],
-    output = process.argv[3];
-    
-if (!mainfile) {
-    console.log('\x1b[1mUsage\x1b[0m: import input_file [output_file] [options]\n');
-    console.log('If an output file is not provided, a server will be started at');
+    fs = require('fs'),
+    optparse = require('optparse');
+
+var switches = [
+    ['-h', '--help', "shows this help section"],
+    ['-p', '--port NUMBER', "start a server to serve the file on this port"],
+];
+
+var parser = new optparse.OptionParser(switches);
+
+var printUsage = function() {
+    parser.banner = "Usage: import input_file [output_file] [options]"
+    console.log(parser.toString());
+    console.log('\nIf an output file is not provided, a server will be started at');
     console.log('at the port provided by the --port or -p option or port 8080 by default.')
-    process.exit();
+};
+
+parser.on('help', function() {
+    printUsage();
+    process.exit(1);
+});
+
+var mainfile;
+parser.on(0, function(arg) {
+    mainfile = arg;
+});
+
+var output;
+parser.on(1, function(arg) {
+    output = arg;
+});
+
+var port;
+parser.on("port", function(name, value) {
+    port = value;
+});
+
+parser.parse(process.argv.splice(2));
+
+if (!mainfile) {
+    printUsage();
+    process.exit(1);
 }
 
-if (output && output != '-p' && output != '--port') {
+if (output) {
     compile(mainfile, function(err, code) {
         if (err) throw err;
         fs.writeFile(output, code);
     });
 }
-else {
-    var port = output == '-p' || output == '--port' ? +process.argv[4] : 8080;
+
+if (port || !output) {
+    port = port || 8080;
     
     http.createServer(function(req, res) {
         res.writeHead(200);
@@ -35,5 +68,5 @@ else {
         
     }).listen(port);
     
-    console.log('Server listening at port ' + port);
+    console.log("Server listening at http://localhost:" + port);
 }
